ef1083ac39bfb9f227f80cf9f3c7e85e4d031b34
DiandraRifqiM <LuxamSky@gmail.com>
2023-03-19 21:12:58 +0700

    again

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import spinner from "../public/spinner.gif";
 
 const Spinner = () => {
   return (
     <>
-      <Image className="w-[200px] m-auto block" src={spinner} alt="loading.." />
+      <Image
+        className="w-[200px] m-auto block"
+        src="../public/spinner.gif"
+        alt="loading.."
+      />
     </>
   );
 };
